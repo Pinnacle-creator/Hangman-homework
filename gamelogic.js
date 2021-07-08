0a46df3cc050627b881456b580426af8e370ffab
Pinnacle-creator <joerir@gmail.com>
2021-07-08 20:46:24 +0200

    eerste functie opgelost

--- gamelogic.js
+++ gamelogic.js
@@ -1,6 +1,22 @@
 function displayWordSoFar(word, guesses) {
-  // WRITE ME
-}
+  let string = "";
+
+  const array = word.split("");
+  console.log(array);
+  for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    if (guesses.includes(array[i])) {
+      string += array[i] + " ";
+    } else string += "_ ";
+  }
+  console.log(string);
+  return string
+
+//  * Hoe kan ik iedere letter afzonderlijk bekijken
+//  * Hoe kan ik automatisch over alle letters van het woord itereren
+//  * Zit deze letter in de guesses array, zo ja print letter zelf
+//  * Anders underscore
+
 
 function isGameWon(word, guesses) {
   // WRITE ME
